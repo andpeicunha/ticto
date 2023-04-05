205a560ace51c23a79fdd78ceee2fcd605e9c6cd
André Peixoto <andrepeixoto@gmail.com>
2023-04-05 05:53:30 -0300

    fix: :bug: default export

--- pages/src/components/StList.tsx
+++ pages/src/components/StList.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+const Container = styled.div`
   background-color: blue;
   width: 10vw;
   height: 10vh;
   display: flex;
   flex-direction: column;
 `
+export default Container
