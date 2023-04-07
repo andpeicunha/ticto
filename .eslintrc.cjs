6f9ec5d3e852fa9be0c159466031f1738e602e4f
André Peixoto <andrepeixoto@gmail.com>
2023-04-07 07:11:14 -0300

    disable rules eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,10 @@ module.exports = {
         // 'some-other-config-you-use',
         // 'prettier'
     ],
+    rules: {
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off"
+    },
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
