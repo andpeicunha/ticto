e5bac5a1c278aa22acb8a0c4acf2c453b1fb0ca9
André Peixoto <andrepeixoto@gmail.com>
2023-04-07 07:48:21 -0300

    .

--- pages/lib/dbConnect.js
+++ pages/lib/dbConnect.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
-const connectMongo = async () => mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
+const URI = 'mongodb://andrepeixoto:5pM87zGweaK0cd1T@ac-rpoiokd-shard-00-00.1zspnmi.mongodb.net:27017,ac-rpoiokd-shard-00-01.1zspnmi.mongodb.net:27017,ac-rpoiokd-shard-00-02.1zspnmi.mongodb.net:27017/test?replicaSet=atlas-60f9j7-shard-0&ssl=true&authSource=admin'
+
+// const connectMongo = async () => mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
+const connectMongo = async () => mongoose.connect(URI);
 export default connectMongo;
 
